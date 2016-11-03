dab9e7eb2bb6982e726cd5f0c5c38cfa635a9798
Graham Lee <wgin.lee@gmail.com>
2016-11-03 18:33:30 +0000

    refactor as forEach

--- waldo.js
+++ waldo.js
@@ -1,10 +1,16 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
+
+  arr.forEach(function (e, i){
+    if (e === "Waldo") {
+      found(i);
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
